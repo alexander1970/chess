2708e423fd87e80a897b5f4058293e5bfdeba01f
alexander_shloma <alexander_shloma@mail.ru>
2023-07-11 21:37:40 +0300

    доска 8x8

--- js/chess.js
+++ js/chess.js
@@ -1,5 +1,18 @@
+let disSquare = '<div id="s$coord" class="squate $color"></div>';
+
 $(function () {
+  addSquares();
+});
+
+function addSquares() {
+  console.log('addSquares');
   $('.board').html('');
-  $('.board').append('<div class="squate white"></div>');
-  $('.board').append('<div class="squate black"></div>');
-})
+  for (let coord = 0; coord < 64; coord++)
+    $('.board').append(disSquare
+      .replace('$color',
+        isBlackSquareAt(coord) ? 'black' : 'white'));
+}
+
+function isBlackSquareAt(coord) {
+  return (coord % 8 + Math.floor(coord / 8)) % 2;
+}
