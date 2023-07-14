841987c74831e66bc07469c01ed3f43248f7a2b1
alexander_shloma <alexander_shloma@mail.ru>
2023-07-14 10:24:37 +0300

    сесия не рботает

--- chess.php
+++ chess.php
@@ -1,6 +1,11 @@
 <?php
 
+session_start();
+if (strlen($_SESSION['map']) != 64)
+  $_SESSION['map'] = 'rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR';
+
 if (isset($_GET['moveFigure']))
   echo 'rnbqkbnrpppppppp1111111111111111111111111111111PPPPPPPP1RNBQKBNR';
-else
-  echo 'rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR';
+
+if (isset($_GET['moveFigures']))
+  echo $_SESSION['map'];
