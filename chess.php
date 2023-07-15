35c00e45cf777648f627cfeb6cd57ac682e8c992
alexander_shloma <alexander_shloma@mail.ru>
2023-07-15 15:51:42 +0300

    Storage

--- chess.php
+++ chess.php
@@ -1,19 +1,25 @@
 <?php
 
-session_start();
+include 'class/Storage.php';
+include 'class/SessionStorage.php';
+
+$storage = new SessionStorage('map');
+
 if (isset($_GET['newFigures'])) {
-  $_SESSION['map'] = 'rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR';
-  echo $_SESSION['map'];
+  $storage->save('rnbqkbnrpppppppp11111111111111111111111111111111PPPPPPPPRNBQKBNR');
+  echo $storage->load();
 }
 
 if (isset($_GET['getFigures']))
-  echo $_SESSION['map'];
+  echo $storage->load();
 
 if (isset($_GET['moveFigure'])) {
   $frCoord = $_GET['frCoord'];
   $toCoord = $_GET['toCoord'];
-  $figure = $_SESSION['map'][$frCoord];
-  $_SESSION['map'][$frCoord] = '1';
-  $_SESSION['map'][$toCoord] = $figure;
-  echo $_SESSION['map'];
+  $figures = $storage->load();
+  $figure = $figures[$frCoord];
+  $figures[$frCoord] = '1';
+  $figures[$toCoord] = $figure;
+  $storage->save($figures);
+  echo $storage->load();
 }
---
+++ class/SessionStorage.php
@@ -0,0 +1,22 @@
+<?php
+
+class SessionStorage implements Storage
+{
+  public $name;
+
+  function __construct($name)
+  {
+    $this->name = $name;
+    session_start();
+  }
+
+  function save($figures)
+  {
+    $_SESSION[$this->name] = $figures;
+  }
+
+  function load()
+  {
+    return $_SESSION[$this->name];
+  }
+}
